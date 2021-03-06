e6b4db59a73cb5b5190a7116db7d9e387d5fef23
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2021-03-06 11:35:23 +1100

    Update process_climate_forcing.sh

--- shell/process_climate_forcing.sh
+++ shell/process_climate_forcing.sh
@@ -4,13 +4,14 @@ temp_1850=historical_ssp245_r1i1p1f1_K_1850_2100_historical_ssp245_regrid_setgri
 
 forcing_lpj=/g/data/w35/lt0205/research/lpj_guess/forcing/climdata/CMIP6
 
-declare -a model_list=('CanESM5' 'CESM2-WACCM' 'CMCC-CM2-SR5' 'EC-Earth3' 
+declare -a model_list=('CanESM5' 'CESM2-WACCM' 'CMCC-CM2-SR5' 'EC-Earth3'
                        'EC-Earth3-Veg' 'GFDL-CM4' 'IITM-ESM' 'INM-CM4-8'
-                       'INM-CM5-0' 'IPSL-CM6A-LR' 'KIOST-ESM' 'MIROC6' 
-                       'MPI-ESM1-2-HR' 'MPI-ESM1-2-LR' 'MRI-ESM2-0' 
+                       'INM-CM5-0' 'IPSL-CM6A-LR' 'KIOST-ESM' 'MIROC6'
+                       'MPI-ESM1-2-HR' 'MPI-ESM1-2-LR' 'MRI-ESM2-0'
                        'NorESM2-LM' 'NorESM2-MM')
-                       
+
 for model in "${model_list[@]}"; do
+    echo ${model}
     for var in pr; do
         cdo -L -b F64 -remapycon,ozgrid.txt \
             ${var}/${model}/r1i1p1f1/${var}_${model}_${prec_1850} \
@@ -18,7 +19,7 @@ for model in "${model_list[@]}"; do
         ncpdq -F -O -a lat,lon,time \
               prec/prec_${model}_SSP245_r1i1p1f1_K_1850_2100.nc \
               ${forcing_lpj}/prec_${model}_SSP245_r1i1p1f1_K_1850_2100.nc
-    done 
+    done
     for var in rsds; do
         cdo -L -b F64 -remapycon,ozgrid.txt \
             ${var}/${model}/r1i1p1f1/${var}_${model}_${insol_1850} \
@@ -26,7 +27,7 @@ for model in "${model_list[@]}"; do
         ncpdq -F -O -a lat,lon,time \
               insol/insol_${model}_SSP245_r1i1p1f1_K_1850_2100.nc \
               ${forcing_lpj}/insol_${model}_SSP245_r1i1p1f1_K_1850_2100.nc
-    done 
+    done
     for var in tas; do
         cdo -L -b F64 -remapycon,ozgrid.txt \
             ${var}/${model}/r1i1p1f1/${var}_${model}_${temp_1850} \
@@ -34,5 +35,5 @@ for model in "${model_list[@]}"; do
         ncpdq -F -O -a lat,lon,time \
               temp/temp_${model}_SSP245_r1i1p1f1_K_1850_2100.nc \
               ${forcing_lpj}/temp_${model}_SSP245_r1i1p1f1_K_1850_2100.nc
-    done 
-done 
+    done
+done
