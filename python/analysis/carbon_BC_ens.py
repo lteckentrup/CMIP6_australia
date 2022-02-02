ace23a930a8de23006a983746f20399396b96704
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-02-02 14:44:13 +1100

    Update carbon_BC_ens.py

--- python/analysis/carbon_BC_ens.py
+++ python/analysis/carbon_BC_ens.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.lines import Line2D
 from read_in import model_names_bounding
+from matplotlib.gridspec import GridSpec
 
 fig=plt.figure(figsize=(10,16))
 
-fig.subplots_adjust(hspace=0.3)
+fig.subplots_adjust(hspace=0.8)
 fig.subplots_adjust(wspace=0.28)
 fig.subplots_adjust(right=0.8)
 fig.subplots_adjust(left=0.10)
-fig.subplots_adjust(bottom=0.08)
+fig.subplots_adjust(bottom=0.05)
 fig.subplots_adjust(top=0.95)
 
 plt.rcParams['text.usetex']=False
@@ -21,16 +22,18 @@ plt.rcParams['legend.fontsize']=12
 plt.rcParams['xtick.labelsize']=11
 plt.rcParams['ytick.labelsize']=11
 
-ax1=fig.add_subplot(4,2,1)
-ax2=fig.add_subplot(4,2,2)
-ax3=fig.add_subplot(4,2,3)
-ax4=fig.add_subplot(4,2,4)
-ax5=fig.add_subplot(4,2,5)
-ax6=fig.add_subplot(4,2,6)
-ax7=fig.add_subplot(4,2,7)
-ax8=fig.add_subplot(4,2,8)
+gs=GridSpec(13,2)
 
-axes=[ax1,ax2,ax3,ax4,ax5,ax6]
+ax1=fig.add_subplot(gs[0:3,:1])
+ax2=fig.add_subplot(gs[0:3,1:])
+ax3=fig.add_subplot(gs[4:7,:1])
+ax4=fig.add_subplot(gs[4:7,1:])
+ax5=fig.add_subplot(gs[7:10,:1])
+ax6=fig.add_subplot(gs[7:10,1:])
+ax7=fig.add_subplot(gs[10:,:1])
+ax8=fig.add_subplot(gs[10:,1:])
+
+axes=[ax3,ax4,ax5,ax6,ax7,ax8]
 
 color=['tab:blue', 'tab:orange','tab:green','tab:red','tab:purple','tab:brown',
        '#515151']
@@ -53,7 +56,7 @@ rolling['min'] = rolling.drop(columns=['CRUJRA']).min(axis=1)
 rolling['max'] = rolling.drop(columns=['CRUJRA']).max(axis=1)
 
 for ax in axes:
-    if ax == ax6:
+    if ax == ax8:
         label_cru='_nolegend_'
         label_spread='_nolegend_'
     else:
@@ -87,10 +90,10 @@ def plot_wurst_BC(ax, model,marker, addition):
             ls = '--'
 
         ax.plot(rolling,label=mt,color=c,lw=lw,ls=ls)
-        ax7.scatter(p+addition,df_CTotal_diff,color=c,marker=marker)
-        ax8.scatter(p+addition,df_NEE_diff,color=c,marker=marker)
+        ax1.scatter(p+addition,df_CTotal_diff,color=c,marker=marker)
+        ax2.scatter(p+addition,df_NEE_diff,color=c,marker=marker)
 
-axes_BC = [ax1,ax2,ax3,ax4,ax5]
+axes_BC = [ax3,ax4,ax5,ax6,ax7]
 marker = ['o', 'x', 's', '+', 'v']
 addition = [0.3,-0.3,0,0.1,-0.1]
 
@@ -117,16 +120,16 @@ def plot_wurst_ens(method, selection, color, position, marker):
     else:
         label = method
 
-    ax6.plot(rolling,label=label,color=color,lw=1.5,ls='--')
+    ax8.plot(rolling,label=label,color=color,lw=1.5,ls='--')
 
     selection_methods = ['full', 'skill', 'independence', 'bounding']
     selection_markers = ['o', 'D', '_', 'h']
 
     for sme, sma in zip(selection_methods, selection_markers):
         if sme == 'full':
-            ax7.scatter(position,df_CTotal_diff[sme],color=color,
+            ax1.scatter(position,df_CTotal_diff[sme],color=color,
                         marker=sma,facecolors='none')
-            ax8.scatter(position,df_NEE_diff[sme],color=color,
+            ax2.scatter(position,df_NEE_diff[sme],color=color,
                         marker=sma,facecolors='none')
         else:
             if method == 'Weighted':
@@ -136,9 +139,9 @@ def plot_wurst_ens(method, selection, color, position, marker):
                     addition=-0.2
                 else:
                     addition=0.2
-                ax7.scatter(position+addition,df_CTotal_diff[sme],
+                ax1.scatter(position+addition,df_CTotal_diff[sme],
                             color=color,marker=sma)
-                ax8.scatter(position+addition,df_NEE_diff[sme],
+                ax2.scatter(position+addition,df_NEE_diff[sme],
                             color=color,marker=sma)
 
 ens_methods = ['Uniform', 'Weighted', 'Random_Forest']
@@ -150,9 +153,9 @@ for em, ec, ep in zip(ens_methods, ens_colors, ens_pos):
 
 axes_all = [ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8]
 title_num = ['a)','b)','c)','d)','e)','f)','g)','h)']
-title_name = ['EC-Earth3-Veg', 'INM-CM4-8', 'KIOST-ESM', 'MPI-ESM1-2-HR',
-              'NorESM2-MM', 'Ensemble average', 'Average $\mathrm{C_{Total}}$',
-              'IAV NBP']
+title_name = ['Average $\mathrm{C_{Total}}$', 'IAV NBP', 'EC-Earth3-Veg',
+              'INM-CM4-8', 'KIOST-ESM', 'MPI-ESM1-2-HR', 'NorESM2-MM',
+              'Ensemble average']
 
 for aa,tnu,tna in zip(axes_all,title_num,title_name):
     aa.set_title(tnu, loc='left')
@@ -163,14 +166,14 @@ for aa,tnu,tna in zip(axes_all,title_num,title_name):
 
     aa.axhline(color='k', alpha=0.5,lw=0.5)
 
-for a in (ax1,ax3,ax5):
+for a in (ax3,ax5,ax7):
     a.set_ylabel('$\Delta \mathrm{C_{Total}}$ [PgC]')
 
-ax7.set_ylabel('$\mathrm{\Delta C_{\mathrm{Total,\mu,CMIP6-CRUJRA}}}$ [PgC]')
-ax8.set_ylabel('$\mathrm{\Delta NBP_{\mathrm{\sigma,CMIP6-CRUJRA}}}$ [PgC]')
+ax1.set_ylabel('$\mathrm{\Delta C_{\mathrm{Total,\mu,CMIP6-CRUJRA}}}$ [PgC]')
+ax2.set_ylabel('$\mathrm{\Delta NBP_{\mathrm{\sigma,CMIP6-CRUJRA}}}$ [PgC]')
 
-ax4.legend(loc='upper center', bbox_to_anchor=(1.3, 1.1), ncol=1, frameon=False)
-ax6.legend(loc='upper center', bbox_to_anchor=(1.3, 0.8), ncol=1, frameon=False)
+ax6.legend(loc='upper center', bbox_to_anchor=(1.3, 1.7), ncol=1, frameon=False)
+ax8.legend(loc='upper center', bbox_to_anchor=(1.32, 0.8), ncol=1, frameon=False)
 
 custom_markers = [Line2D([0], [0], marker='o', linestyle='', color='k'),
                   Line2D([0], [0], marker='x', linestyle='', color='k'),
@@ -183,17 +186,17 @@ custom_markers = [Line2D([0], [0], marker='o', linestyle='', color='k'),
                   Line2D([0], [0], marker='_', linestyle='', color='k'),
                   Line2D([0], [0], marker='h', linestyle='', color='k')]
 
-ax8.legend(custom_markers, ['EC-Earth3-Veg', 'INM-CM4-8', 'KIOST-ESM',
+ax2.legend(custom_markers, ['EC-Earth3-Veg', 'INM-CM4-8', 'KIOST-ESM',
                             'MPI-ESM1-2-HR', 'NorESM2-MM', 'Full', 'Skilled',
                             'Independence', 'Bounding'],
-           loc='upper center', bbox_to_anchor=(1.3, 1.1),frameon=False)
+           loc='upper center', bbox_to_anchor=(1.33, 1.1),frameon=False)
 
 bc_methods = ['original', 'SCALING', 'MAV', 'QM', 'CDFt', 'dOTC', 'MRec',
               'Uniform', 'I+S', 'RF']
 
-for a in (ax1,ax2,ax3,ax4):
+for a in (ax3,ax4,ax5,ax6):
     a.set_xticklabels([])
-for a in (ax7, ax8):
+for a in (ax1, ax2):
     a.set_xticks([1,2.5,3.5,4.5,5.5,6.5,7.5,9,10,11])
     a.set_xticklabels(bc_methods,rotation = 45, ha='right')
 
@@ -204,4 +207,3 @@ fig.align_ylabels()
 
 # plt.show()
 plt.savefig('carbon_BC_ens.pdf')
-# plt.savefig('carbon_result_BC_monthly.pdf')
