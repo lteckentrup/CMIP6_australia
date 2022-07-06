b87211e57f787f41c72d3997e12468de6e803615
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-07-06 09:43:13 +0200

    Update BC_delta.py

--- python/analysis/figures/BC_delta.py
+++ python/analysis/figures/BC_delta.py
@@ -37,8 +37,8 @@ color=['tab:blue', 'tab:orange','tab:green','tab:red','tab:purple','tab:brown',
 GCMs_bounding = ['INM-CM4-8', 'NorESM2-MM', 'MPI-ESM1-2-HR', 'KIOST-ESM',
                  'EC-Earth3-Veg']
 
-def readin(var, method, GCM):
-    if GCM == 'CRUJRA':
+def readin(var, method):
+    if method == 'Reanalysis':
         df = pd.read_csv('../LPJ_monthly_corrected/original_csv/'+var+'_full.csv',
                          index_col='Year')
         return(df_CRUJRA)
@@ -46,8 +46,6 @@ def readin(var, method, GCM):
         if method in ['original', 'Scaling', 'MAV', 'QM', 'CDF-t', 'R2D2', 'dOTC']:
             df = pd.read_csv('../LPJ_monthly_corrected/'+method+'_csv/'+var+'_full.csv',
                              index_col='Year')
-            if method == 'original':
-                df = df.drop(columns=['CRUJRA'])
         elif method == 'Weighting':
             df = pd.read_csv('../LPJ_ensemble_averages/'+method+'_csv/'+var+'.csv',
                              index_col='Year')
@@ -63,29 +61,28 @@ def rolling_avg(df):
     rolling = df_anomaly.rolling(window=30,center=True).mean()
     return(rolling)
 
-df_CTotal = pd.read_csv('../LPJ_monthly_corrected/original_csv/CTotal_full.csv',
-                        index_col='Year')
+def spread(var):
+    df = readin(var, 'original')
 
-df_CTotal_CRUJRA = df_CTotal['CRUJRA'][-30:].mean()
+    rolling = rolling_avg(df)
+    rolling['min'] = rolling.drop(columns=['CRUJRA']).min(axis=1)
+    rolling['max'] = rolling.drop(columns=['CRUJRA']).max(axis=1)
 
-rolling = rolling_avg(df_CTotal)
-rolling['min'] = rolling.drop(columns=['CRUJRA']).min(axis=1)
-rolling['max'] = rolling.drop(columns=['CRUJRA']).max(axis=1)
+    for ax in axes:
+        label_cru='LG$_\mathrm{CRUJRA}$'
+        label_spread='Raw ensemble\nspread'
 
-for ax in axes:
-    label_cru='LG$_\mathrm{CRUJRA}$'
-    label_spread='Raw ensemble\nspread'
-
-    ax.fill_between(rolling.reset_index()['Year'], rolling['min'], rolling['max'],
-                    label=label_spread, color='tab:grey', alpha=0.15)
-    ax.plot(rolling['CRUJRA'],label=label_cru,color='k',lw=3,ls='-')
+        ax.fill_between(rolling.reset_index()['Year'], rolling['min'], 
+                        rolling['max'], label=label_spread, color='tab:grey', 
+                        alpha=0.15)
+        ax.plot(rolling['CRUJRA'],label=label_cru,color='k',lw=3,ls='-')
 
 def plot_BC(ax, GCM, var):
     BC_methods = ['original', 'Scaling', 'MAV', 'QM', 'CDF-t', 'dOTC', 'R2D2']
     position = [1,2.5,3.5,4.5,5.5,6.5,7.5]
 
     for mt,c,p in zip(BC_methods,color,position):
-        df = readin(var, mt, GCM)
+        df = readin(var, mt)
         rolling = rolling_avg(df[GCM])
 
         if mt == 'original':
@@ -99,12 +96,9 @@ def plot_BC(ax, GCM, var):
 
         ax.plot(rolling,label=label,color=c,lw=lw,ls=ls)
 
-for ax, mn in zip(axes_BC, GCMs_bounding):
-    plot_BC(ax, mn, 'CTotal')
-
 ### Plot ensemble averages
 def plot_ENS(method, selection, color, var):
-    df = readin(var, method, '')
+    df = readin(var, method)
     rolling = rolling_avg(df['full'])
 
     if method == 'Random_Forest':
@@ -119,17 +113,39 @@ def plot_ENS(method, selection, color, var):
 ens_methods = ['Uniform', 'Weighted', 'Random_Forest']
 ens_colors = ['tab:olive', 'tab:pink', 'tab:cyan']
 
+var='prec'
+
+### Plot change in in variable
+spread(var)
+for ax, mn in zip(axes_BC, GCMs_bounding):
+    plot_BC(ax, mn, var)
+
 for em, ec in zip(ens_methods, ens_colors):
-    plot_ENS(em, 'full', ec, 'CTotal')
+    plot_ENS(em, 'full', ec, var)
 
 axes_all = [ax1,ax2,ax3,ax4,ax5,ax6]
 title_num = ['a)','b)','c)','d)','e)','f)']
-title_name = ['LG$_\mathrm{INM-CM4-8}$',
-              'LG$_\mathrm{NorESM2-MM}$',
-              'LG$_\mathrm{MPI-ESM1-2-HR}$',
-              'LG$_\mathrm{KIOST-ESM}$',
-              'LG$_\mathrm{EC-Earth3-Veg}$',
-              'Ensemble Average']
+
+if var == 'CTotal':
+    ylabel = '$\Delta \mathrm{C_{Total}}$ [PgC]'
+    title_name = ['LG$_\mathrm{INM-CM4-8}$',
+                  'LG$_\mathrm{NorESM2-MM}$',
+                  'LG$_\mathrm{MPI-ESM1-2-HR}$',
+                  'LG$_\mathrm{KIOST-ESM}$',
+                  'LG$_\mathrm{EC-Earth3-Veg}$',
+                  'Ensemble Average']
+else:
+    if var == 'temp':
+        ylabel = '$\Delta$ T [$^\circ$C]'
+    elif var == 'prec':
+        ylabel = '$\Delta$ PPT [mm]'
+
+    title_name = ['INM-CM4-8',
+                  'NorESM2-MM',
+                  'MPI-ESM1-2-HR',
+                  'KIOST-ESM',
+                  'EC-Earth3-Veg',
+                  'Ensemble Average']
 
 for aa,tnu,tna in zip(axes_all,title_num,title_name):
     aa.set_title(tnu, loc='left')
@@ -144,7 +160,7 @@ for a in (ax1,ax2,ax3,ax4):
     a.set_xticklabels([])
 
 for a in (ax1,ax3,ax5):
-    a.set_ylabel('$\Delta \mathrm{C_{Total}}$ [PgC]')
+    a.set_ylabel(ylabel)
 
 custom_markers = [Patch(facecolor='tab:grey', edgecolor='tab:grey', alpha=0.15),
                   Line2D([0], [0], linestyle='-', lw=3, color='k'),
@@ -174,9 +190,7 @@ ax5.legend(custom_markers, ['Raw ensemble spread',
 
            loc='upper center', bbox_to_anchor=(1.0, -0.25),ncol=4, frameon=False)
 
-
 fig.align_ylabels()
 
-# plt.show()
-plt.savefig('BC_delta.pdf')
-# plt.savefig('carbon_result_BC_monthly.pdf')
+plt.show()
+# plt.savefig('BC_delta.pdf')
